0cddfd15c9fd73a8d58c692779a96d430572b882
Maxim <bekrinmks@gmail.com>
2023-02-11 11:13:58 +0300

    first_finish#2

--- components/course_eng-for-travel/course_eng-for-travel.js
+++ components/course_eng-for-travel/course_eng-for-travel.js
@@ -1,4 +1,5 @@
 import DropDownSlowly from './course-program/course-program.js';
 import SlickSlider from './structure_lesson.js';
+import FreeLesson from '../modal/freeLesson/FreeLesson.js';
 DropDownSlowly.addListenner();
 SlickSlider.addEventListenner();
