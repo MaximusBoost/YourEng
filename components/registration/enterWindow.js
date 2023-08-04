ce5d4400253d134259ff493a550ea71941327171
max7891112 <max.alkhimovich@gmail.com>
2023-08-05 08:41:51 +1000

    working on autorization

---
+++ components/registration/enterWindow.js
@@ -0,0 +1,144 @@
+import TechnicalFunctions from "../../technicalFunctions/TechnicalFunctions";
+
+class FormEnter{
+    constructor() {
+        this.checker = document.querySelector('.registration__checker')
+    }
+    sendForm() {
+        const form = document.querySelector('.form-send');
+        const container = document.querySelector('.container-send')
+        form.addEventListener('submit', async (event) => {
+            event.preventDefault();
+
+            let error = formValidate();
+
+            let formData = new FormData(form);
+
+            if(error == 0) {
+                container.classList.add('_sending');
+                
+                setTimeout(() => {
+                    let response = fetch('sendmail.php', { // адрес сервера для отправки запроса
+                        method: 'POST',
+                        body: formData,
+                    });
+                    if(response.ok) {
+                        let result = response.json(); // await
+                        let formReq = document.querySelectorAll('._form__req');
+                        formReq.forEach((input) => {
+                            if(input.classList.contains('modal__input_name')) {
+                                input.placeholder = 'Логин'
+                                if(this.checker.getAttribute('checked')) {
+                                    localStorage.setItem('login', input.value)
+                                    // localStorage.setItem('checker', 'true')
+                                }   
+                            } else if(input.classList.contains('modal__input_password')) {
+                                input.placeholder = 'Пароль'
+                            } else if(input.classList.contains('modal__input_phone')) {
+                                input.placeholder = '+7 (___)-___-__-__'
+                            } else if(input.classList.contains('modal__input_email')) {
+                                input.placeholder = 'example@mail.ru'
+                            }
+                        })
+                        alert(result.message);
+                        form.reset();
+                        container.classList.remove('_sending');
+                    } else {
+                        let formReq = document.querySelectorAll('._form__req');
+                        formReq.forEach((input) => {
+                            if(input.classList.contains('modal__input_name')) {
+                                input.placeholder = 'Логин'
+                                if(this.checker.getAttribute('checked')) {
+                                    localStorage.setItem('login', input.value)
+                                    // localStorage.setItem('checker', 'true')
+                                }
+                            } else if(input.classList.contains('modal__input_password')) {
+                                input.placeholder = 'Пароль'
+                            } else if(input.classList.contains('modal__input_phone')) {
+                                input.placeholder = '+7 (___)-___-__-__'
+                            } else if(input.classList.contains('modal__input_email')) {
+                                input.placeholder = 'example@mail.ru'
+                            }
+                        })
+                        alert('Ошибка отправки данных на сервер')
+                        container.classList.remove('_sending')
+                        
+                    };
+                    form.reset();
+                },1000);
+                
+            } else {
+                alert('Заполните обязательные поля');
+            };
+        });
+
+        
+
+        function formValidate() {
+            let error = 0;
+            let formReq = document.querySelectorAll('._form__req');
+            for(let i = 0; i< formReq.length; i++) {
+                let input = formReq[i];
+                TechnicalFunctions.formRemoveError(input);
+                if(input.value == '') {
+                    formAddError(input);
+                    error++;
+                }
+                 else if(input.classList.contains('modal__input_name') && input.value.length < 8) {
+                    formAddError(input);
+                    error++;
+                    input.value = ''
+                    input.placeholder = 'Логин должен содержать более 8 символов'
+                }
+                else if(input.classList.contains('modal__input_password') && input.value.length < 8) {
+                    formAddError(input);
+                    error++;
+                    input.value = ''
+                    input.placeholder = 'Пароль должен содержать более 8 символов'
+                }
+            };
+            return error;
+        };
+            
+        function formAddError(input) {
+            input.classList.add('_error');
+            if(input.placeholder == 'Логин') {
+                input.placeholder = 'Введите логин';
+            } else if(input.placeholder == 'Пароль') {
+                input.placeholder = 'Введите пароль'
+            } else if(input.placeholder == '+7 (___)-___-__-__') {
+                input.placeholder = 'Введите номер телефона'
+            } else if(input.placeholder == 'example@mail.ru') {
+                input.placeholder = 'Введите почту'
+            }
+        };
+
+        TechnicalFunctions.addListenerForDataInput();
+    };
+
+    chekerListener() {
+        if(this.checker) {
+            this.checker.addEventListener('click', () => {
+                if(this.checker.getAttribute('checked')) {
+                    this.checker.removeAttribute('checked')
+                } else {
+                    this.checker.setAttribute('checked', 'true')
+                }
+            })
+        }
+    }
+
+    autoFillLogin() {
+        let input = document.querySelector('.modal__input_name')
+        if(localStorage.getItem('login') && !input.classList.contains('_register')) {
+            let inputValue = localStorage.getItem('login')
+            input.value = inputValue
+            this.checker.checked = 'true'
+        }
+    }
+};
+
+let callThisFunc = new FormEnter;
+callThisFunc.sendForm()
+callThisFunc.chekerListener()
+callThisFunc.autoFillLogin()
